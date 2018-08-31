e3d8e8a9f7267e3b5de662e388e056f2d5264479
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-08-31 18:07:17 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,18 @@
-class Spaceship {}
+class Spaceship {
+
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged",phasersCharge = "uncharged"){
+    this.name = name;
+    this.crew = crew;
+    crew.forEach(function(crewMember){
+      console.log(this);
+      crewMember.currentShip = this;
+    }.bind(this))
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = cloaked;
+    this.warpDrive = warpDrive;
+    this.phasersCharge = phasersCharge;
+    this.docked = this.crew.length === 0? true : false ;
+  }
+
+}
